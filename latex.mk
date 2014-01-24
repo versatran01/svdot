f15849b65124064a400f4246ddf768fb4c4eb6ac
Chao Qu <quchao@seas.upenn.edu>
2014-01-23 19:17:22 -0500

    add makefile for latex

---
+++ latex.mk
@@ -0,0 +1,13 @@
+FILE=main
+
+all: $(FILE).tex
+	rubber -d $<
+
+edit:
+	texmaker $(FILE).tex &
+
+test:
+	evince $(FILE).pdf &
+
+clean:
+	rm -f *.aux *.log *.out *.pdf *.pdfsync *.synctex.gz *.blg *.bbl
