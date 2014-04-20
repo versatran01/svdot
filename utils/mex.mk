6fdf75cfd4815c87dee880939b04a0e4602cfe42
Chao Qu <quchao@seas.upenn.edu>
2014-04-20 15:56:40 -0400

    various update and clean

--- utils/mex.mk
+++ utils/mex.mk
@@ -1,15 +1,15 @@
-MEX = mex
-CXX ?= g++
-CC ?= gcc
+MEX=mex
+CXX?=g++
+CC?=gcc
 
-CFLAGS = -fPIC
-CXXFLAGS = -fPIC
+CFLAGS=-fPIC
+CXXFLAGS=-fPIC
 
-MEX = mex
-MEX_OPTION = CC\#$(CC) CFLAGS\#"$(CFLAGS)" \
+MEX=mex
+MEX_OPTION=CC\#$(CC) CFLAGS\#"$(CFLAGS)" \
 						 CXX\#$(CXX) CXXFLAGS\#"$(CXXFLAGS)" \
 					   -Wl,--as-needed -O -outdir build
-MEX_EXT = $(shell mexext)
+MEX_EXT=$(shell mexext)
 
 CFLAGS_OPENCV=`pkg-config opencv --cflags --libs`
 
