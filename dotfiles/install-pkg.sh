f8c0e170c55e81b4fa81c31a1df4e2c9a6c91617
Chao Qu <quchao@seas.upenn.edu>
2014-04-24 16:42:27 -0400

    clean up again

--- dotfiles/install-pkg.sh
+++ dotfiles/install-pkg.sh
@@ -24,6 +24,7 @@ PACKAGES=(ack-grep
           openjdk-7-jdk
           openjdk-7-jre
           openshot
+          pep8
           pkg-config
           python-matplotlib
           python-numpy
@@ -40,7 +41,7 @@ PACKAGES=(ack-grep
 #Stop if a command ends in an error
 set -e
 
-#Check to see if we are unning with root privileges
+#Check to see if we are running with root privileges
 if [[ $(id -u) -ne 0 ]] ; then
   echo "Please run this script as root (eg using sudo)"
   exit 1
