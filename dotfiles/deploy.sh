845df1435f335475a79d2e9500d20979ea28f21e
Chao Qu <quchao@seas.upenn.edu>
2015-01-27 14:53:45 -0500

    add deploy

---
+++ dotfiles/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FILES=(bash_local
+bash_aliases
+bash_functions
+vimrc
+bundles
+tmuxline
+tmux.conf
+gitconfig
+gitignore
+inputrc
+promptline.sh
+)
+
+for file in "${FILES[@]}"
+do
+    echo Linking "$file" to $HOME
+    ln -s $(pwd)/$file ~/.$file
+done
